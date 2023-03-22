8cae06a338019fdd6bdeb64fad20b33d8417b948
linhhoacao <124118236+linhhoacao@users.noreply.github.com>
2023-03-22 08:10:58 +0700

    Merge branch 'main' of https://github.com/azizgaming2001/semiprojectwebqlsv

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -6,6 +6,7 @@
         <div class="table-responsive p-0">
             <table class="table align-items-center mb-0">
                 <thead>
+                    <input> <input>
                     <tr>
                         <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Full Name</th>
                         <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Account Name</th>
