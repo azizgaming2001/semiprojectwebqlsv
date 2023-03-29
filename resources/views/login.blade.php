157bd7b837a8ba5e3cd7af278f209ddf97ca7fda
azizgaming2001 <azizgamingcl@gmail.com>
2023-03-29 10:01:00 +0700

    logo login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,10 +1,12 @@
 @extends('layout.base')
 @section('page_title', 'Login')
 @section('slot')
+<img class="logo_login" height="250px"  src="https://upload.wikimedia.org/wikipedia/vi/1/1d/Logo_%C4%90%E1%BA%A1i_h%E1%BB%8Dc_FPT.png">
 <div class="col-lg-4 col-md-8 col-12 mx-auto mt-5">
     <div class="card z-index-0 fadeIn3 fadeInBottom">
         <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
             <div class="bg-gradient-primary shadow-primary">
+
                 <h4 class="text-white font-weight-bolder text-center mb-0">WELCOME</h4>
             </div>
         </div>
