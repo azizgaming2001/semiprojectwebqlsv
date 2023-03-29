6fc489efc2b06e1a7cb5901c4f9788e775522278
azizgaming2001 <azizgamingcl@gmail.com>
2023-03-29 09:41:37 +0700

    remove input

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -6,7 +6,7 @@
         <div class="table-responsive p-0">
             <table class="table align-items-center mb-0">
                 <thead>
-                    <input> <input>
+
                     <tr>
                         <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Full Name</th>
                         <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Account Name</th>
@@ -26,7 +26,7 @@
                         <td class="text-xs">{{$row->profile->class->name}}</td>
                         <td class="align-middle">
                             <a class="text-secondary font-weight-bold text-xs"
-                                href="{{route('students.edit', ['id' => $row->id])}}">Change</a> | 
+                                href="{{route('students.edit', ['id' => $row->id])}}">Change</a> |
                             <a class="text-secondary font-weight-bold text-xs"
                                 href="{{route('students.delete', ['id' => $row->id])}}">Delete</a>
                         </td>
