518ab26c28c3f342fca08d25727be5ed0d6236ab
azizgaming2001 <azizgamingcl@gmail.com>
2023-04-03 16:32:33 +0700

    add layout subject

---
+++ resources/views/subjects/index.blade.php
@@ -0,0 +1,39 @@
+@extends('layout.base')
+@section('page_title', 'Subjects')
+@section('slot')
+<div class="card">
+    <div class="card-body px-0 pb-2">
+        <div class="table-responsive p-0">
+            <table class="table align-items-center mb-0">
+                <thead>
+                    <tr>
+                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Name Subject</th>
+                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Subject ID</th>
+                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Semester</th>
+                        <th class="text-secondary opacity-7"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse($rows as $row)
+                    <tr>
+                        <td class="text-xs">{{$row->name}}</td>
+                        <td class="text-xs">{{$row->code}}</td>
+                        <td class="text-xs">{{$row->semester}}</td>
+                        <td class="align-middle">
+                            @if(in_array(auth()->user()->role, ['teacher']))
+                            <a class="text-secondary font-weight-bold text-xs"
+                                href="{{route('subjects.edit', ['id' => $row->id])}}">Change</a> |
+                            <a class="text-secondary font-weight-bold text-xs"
+                                href="{{route('subjects.delete', ['id' => $row->id])}}">Delete</a>
+                            @endif
+                        </td>
+                    </tr>
+                    @empty
+                    <tr><td class="align-middle text-secondary font-weight-bold text-xs">no data discover</td></tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+@stop
